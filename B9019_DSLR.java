a8892b8ba7861c5e825f2f40f4dd4d906846d879
jsl1113 <jiseon1113@naver.com>
2025-07-11 17:51:31 +0900

    B9019

--- B9019_DSLR.java
+++ B9019_DSLR.java
@@ -3,62 +3,65 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+// 1% 에서 틀렸습니다
 public class B9019_DSLR {
-    static int answer = Integer.MAX_VALUE;
-    static boolean[] visited;
-    static String s = "";
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
         while (t-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            visited = new boolean[10_001];
-            answer = Integer.MAX_VALUE;
-            s = "";
-            
-            dfs(b, a, 0, "");
-            System.out.println("answer = " + answer);
-            System.out.println("s = " + s);
-        }
-    }
+            Queue<Object[]> queue = new LinkedList<>();
+            queue.add(new Object[]{a, ""});
+            boolean[] visited = new boolean[10_001];
+            visited[a] = true;
 
-    static void dfs(int n, int a, int cnt, String str) {
-        if (n > 10_000 || n <= 0) return;
+            while (!queue.isEmpty()) {
+                Object[] curr = queue.poll();
 
-        if (n == a) {
-            if (answer > cnt) {
-                answer = cnt;
-                s = str;
+                int next = (int) curr[0];
+                String str = (String) curr[1];
+
+                if(next == b){
+                    sb.append(str).append("\n");
+                    break;
+                }
+
+                int D = (next * 2) % 10_000;
+                int S = next == 0 ? 9999 : next - 1;
+                int L = (next % 1000 / 10) + (next / 1000);
+                int R = (next % 10 * 1000) + (next / 10);
+
+                if(!visited[D]){
+                    visited[D] = true;
+                    queue.add(new Object[]{D, str + "D"});
+                }
+                if(!visited[S]){
+                    visited[S] = true;
+                    queue.add(new Object[]{S, str + "S"});
+                }
+                if(!visited[L]){
+                    visited[L] = true;
+                    queue.add(new Object[]{L, str + "L"});
+                }
+                if(!visited[R]){
+                    visited[R] = true;
+                    queue.add(new Object[]{R, str + "R"});
+                }
             }
-            return;
         }
 
-        visited[n] = true;
-
-        int d = (n / 2) % 10_000;
-        System.out.println("d = " + d);
-        dfs(d, a, cnt + 1, str + "D");
-
-        int s = n + 1;
-        System.out.println("s = " + s);
-        dfs(s, a, cnt + 1, str + "S");
-
-        int l = ((n % 10) * 1000) + (n / 10);
-        System.out.println("l = " + l);
-        dfs(l, a, cnt + 1, str + "L");
-
-        int r = ((n % 1000) * 10) + (n / 1000);
-        System.out.println("r = " + r);
-        dfs(r, a, cnt + 1, str + "R");
-
-        visited[n] = false;
+        System.out.println(sb);
     }
 }
