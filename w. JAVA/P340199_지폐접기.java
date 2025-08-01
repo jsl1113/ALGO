bcb6afd9ade3cdf48fb94a7346aa96b679f27829
jsl1113 <jiseon1113@naver.com>
2025-08-01 18:26:02 +0900

    P340199

---
+++ w. JAVA/P340199_지폐접기.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class P340199_지폐접기 {
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+        int a = wallet[0], b = wallet[1];
+        int c = bill[0], d = bill[1];
+
+        while(true){
+            if((a >= c && b >= d) || (a >= d && b >= c)){
+                break;
+            }
+
+            if(c > d) c = c/2;
+            else d = d/2;
+            answer++;
+        }
+
+        return answer;
+    }
+}
