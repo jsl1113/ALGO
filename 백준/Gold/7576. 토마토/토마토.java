6a14d28acc7166c0eac0296190ccf0e1bd371b8a
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-08-05 10:59:43 +0900

    Update 토마토.java

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+// 처음에 1 인 곳이 한 곳만 있는줄 알고 한 곳만 저장했다가 틀려서
+// 애초에 입력했을 때부터 1인 곳을 큐에 넣어주도록 변경했다.
 public class Main {
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {1, -1, 0, 0};
@@ -12,13 +14,16 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int m = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
+
         int[][] arr = new int[n][m];
         Queue<int[]> queue = new LinkedList<>();
+
         for (int i = 0; i < n; i++) {
             String[] strArray = br.readLine().split(" ");
             for (int j = 0; j < m; j++) {
                 arr[i][j] = Integer.parseInt(strArray[j]);
-                if (arr[i][j] == 1) {
+
+                if (arr[i][j] == 1) { // 익은 토마토라면 큐에 저장
                     queue.add(new int[]{i, j});
                 }
             }
@@ -44,7 +49,7 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (arr[i][j] == 0) {
+                if (arr[i][j] == 0) { // 0 이 하나라도 있으면 모두 익지 않았다
                     System.out.println(-1);
                     return;
                 }
@@ -56,6 +61,6 @@ public class Main {
             day = Math.max(Arrays.stream(arr[i]).max().getAsInt(), day);
         }
 
-        System.out.println(day - 1);
+        System.out.println(day - 1);  // 처음 상태값도 더해져 있기 때문에 -1
     }
 }
