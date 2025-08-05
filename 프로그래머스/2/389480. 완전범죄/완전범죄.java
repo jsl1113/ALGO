49b995a1930952d127f36ff817de749b284cc183
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-08-05 10:02:49 +0900

    [level 2] Title: 완전범죄, Time: 0.26 ms, Memory: 89.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] info, int n, int m) {
+        int answer = 0;
+        int len = info.length;
+        
+        int[] arr = new int[n];
+        Arrays.fill(arr, 121);
+        arr[0] = 0;
+        
+        for(int i=0; i<len; i++){
+            for(int j=n-1; j>=0; j--){
+                arr[j] += info[i][1];
+                
+                if(j-info[i][0] < 0) continue;
+                
+                arr[j] = Math.min(arr[j], arr[j-info[i][0]]);
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            if(arr[i] < m) return i;
+        }
+        
+        return -1;
+    }
+}
