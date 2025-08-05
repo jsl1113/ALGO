bc021b0aad3c638fb99f499603663a063cd4d182
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-08-05 10:04:15 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 14664 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Bag[] arr = new Bag[n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = new Bag(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+        int[] dp = new int[k + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = k; j >= arr[i].w; j--) {
+                dp[j] = Math.max(dp[j], arr[i].v + dp[j - arr[i].w]);
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
+
+    static class Bag {
+        int w, v;
+
+        Bag(int w, int v) {
+            this.w = w;
+            this.v = v;
+        }
+    }
+}
