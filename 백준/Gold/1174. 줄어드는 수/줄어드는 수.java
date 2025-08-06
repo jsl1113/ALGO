9436b6be17a3327e940378fdd93a3a986cab5441
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-08-06 09:32:06 +0900

    [Gold V] Title: 줄어드는 수, Time: 108 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    static int n;
+    static ArrayList<Long> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        if (n > 1023) {
+            System.out.println(-1);
+            return;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            func(i, i);
+        }
+
+        Collections.sort(list);
+        System.out.println(n > list.size() ? -1 : list.get(n - 1));
+    }
+
+    static void func(int cnt, long num) {
+        list.add(num);
+
+        for (int i = 0; i < cnt; i++) {
+            func(i, num * 10 + i);
+        }
+    }
+}
