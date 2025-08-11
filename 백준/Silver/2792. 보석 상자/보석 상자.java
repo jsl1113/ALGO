648fcbbf4f0d1072a59320cc982bd81a66fc0b11
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-08-11 13:22:27 +0900

    [Silver I] Title: 보석 상자, Time: 388 ms, Memory: 34720 KB -BaekjoonHub

---
+++ 백준/Silver/2792. 보석 상자/보석 상자.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int start = 1, end = Arrays.stream(arr).max().getAsInt();
+        while (start < end) {
+            int mid = (start + end) / 2;
+            int cnt = 0;
+            for (int num : arr) {
+                if(num % mid == 0) cnt += (num / mid);
+                else cnt += (num / mid) + 1;
+            }
+
+            if (cnt > n) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        System.out.println(end);
+    }
+}
