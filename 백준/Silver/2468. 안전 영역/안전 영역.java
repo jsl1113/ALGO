1453f606e420e32f04a751fe9eaad738869dabfa
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-08-11 13:14:37 +0900

    [Silver I] Title: 안전 영역, Time: 248 ms, Memory: 18696 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, max = 0, answer = Integer.MIN_VALUE;
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        for (int num = 0; num <= max; num++) {
+            int cnt = 0;
+            visited = new boolean[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (!visited[i][j] && arr[i][j] > num) {
+                        dfs(i, j, num);
+                        cnt++;
+                    }
+                }
+            }
+            answer = Math.max(answer, cnt);
+        }
+
+        System.out.println(answer);
+    }
+
+    static void dfs(int x, int y, int num) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || nx >n-1 || ny < 0 || ny >n-1 || visited[nx][ny] || arr[nx][ny] <= num)
+                continue;
+            dfs(nx, ny, num);
+        }
+    }
+
+    static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                max = Math.max(max, arr[i][j]);
+            }
+        }
+    }
+}
