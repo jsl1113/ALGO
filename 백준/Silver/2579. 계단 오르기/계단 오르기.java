a2b729dd59d44c403efc1739a2feddb390fae827
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-08-11 13:17:42 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+  static int[][] dp = new int[300][2];
+  static int[] arr = new int[300];
+    
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    int n = Integer.parseInt(br.readLine());
+    for(int i = 0; i < n; i++){
+      arr[i] = Integer.parseInt(br.readLine());
+    }
+  
+    dp[0][0] = arr[0];
+    dp[0][1] = arr[0];
+    dp[1][0] = arr[1];
+    dp[1][1] = arr[0] + arr[1];
+    for(int i = 2; i < n; i++){
+      dp[i][0] = Math.max(dp[i-2][0], dp[i-2][1]) + arr[i];
+      dp[i][1] = dp[i-1][0] + arr[i];
+    }
+
+    System.out.println(Math.max(dp[n-1][0], dp[n-1][1]));
+  }
+
+
+  
+}
