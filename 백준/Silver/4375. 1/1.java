bd4adcda434ec322ea877dcf0a9a7113b44d4df6
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-08-11 13:47:32 +0900

    [Silver III] Title: 1, Time: 584 ms, Memory: 140772 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = "";
+        while ((str = br.readLine()) != null) {
+            System.out.println(func(str));
+        }
+    }
+
+    static int func(String s) {
+        BigInteger num = new BigInteger(s);
+        BigInteger div = BigInteger.ZERO;
+
+        while (true) {
+            div = div.multiply(BigInteger.TEN).add(BigInteger.ONE);
+            if (div.mod(num).equals(BigInteger.ZERO)) {
+                break;
+            }
+        }
+
+        return div.toString().length();
+    }
+}
