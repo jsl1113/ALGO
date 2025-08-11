58ae27a309d12521cf68200c7d6c4ecb49c039ee
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-08-11 17:32:16 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 536 ms, Memory: 18204 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static Node root;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        root = new Node(Integer.parseInt(br.readLine()), null, null);
+
+        while (true) {
+            String s = br.readLine();
+            if(s == null || s.equals("")) break;
+            Node node = new Node(Integer.parseInt(s), null, null);
+            addNode(root, node);
+        }
+
+        postOrder(root);
+        System.out.println(sb);
+    }
+
+    static void addNode(Node node, Node next) {
+        if (next.num < node.num) {
+            if(node.left != null) addNode(node.left, next);
+            else node.left = next;
+        } else {
+            if(node.right != null) addNode(node.right, next);
+            else node.right = next;
+        }
+    }
+
+    static void postOrder(Node node) {
+        if(node.left != null) postOrder(node.left);
+        if(node.right != null) postOrder(node.right);
+        sb.append(node.num).append("\n");
+    }
+
+    static class Node {
+        int num;
+        Node left, right;
+
+        Node(int num, Node left, Node right) {
+            this.num = num;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
