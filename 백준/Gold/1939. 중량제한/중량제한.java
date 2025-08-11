44e7f7484e6de713442c5d611be36a676ba38e64
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-08-11 11:03:05 +0900

    [Gold III] Title: 중량제한, Time: 728 ms, Memory: 80016 KB -BaekjoonHub

---
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m, start, end, x, y, answer = -1;
+    static boolean[] visited;
+    static ArrayList<Edge>[] list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        list = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            list[a].add(new Edge(b, c));
+            list[b].add(new Edge(a, c));
+            end = Math.max(end, c);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            visited = new boolean[n + 1];
+            visited[x] = true;
+
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(x);
+            while (!queue.isEmpty()) {
+                int current = queue.poll();
+
+                for (Edge e : list[current]) {
+                    if(visited[e.num] || e.weight < mid) continue;
+
+                    visited[e.num] = true;
+                    queue.add(e.num);
+                }
+            }
+
+            if(visited[y]){
+                start = mid + 1;
+                answer = mid;
+            } else end = mid - 1;
+        }
+
+        System.out.println(answer);
+    }
+
+    static class Edge {
+        int num, weight;
+
+        Edge(int num, int weight){
+            this.num = num;
+            this.weight = weight;
+        }
+    }
+}
