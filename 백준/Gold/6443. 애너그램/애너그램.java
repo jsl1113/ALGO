de892d28abbe3db2af2ffcdda9c6daa40289c07f
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-08-11 17:32:43 +0900

    [Gold V] Title: 애너그램, Time: 1536 ms, Memory: 277292 KB -BaekjoonHub

---
+++ 백준/Gold/6443. 애너그램/애너그램.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    static int n, len;
+    static int[] alphabet;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+
+            alphabet = new int[26];
+            for (int j = 0; j < str.length(); j++) {
+                alphabet[str.charAt(j) - 'a']++;
+            }
+
+            len = str.length();
+            dfs(new ArrayList<>());
+        }
+
+        System.out.println(sb);
+    }
+
+    static void dfs(List<Character> list) {
+        if (list.size() == len) {
+            sb.append(list.stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining())).append("\n");
+            return;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (alphabet[i] > 0) {
+                alphabet[i]--;
+                list.add((char) (i + 'a'));
+                dfs( list);
+                alphabet[i]++;
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+}
