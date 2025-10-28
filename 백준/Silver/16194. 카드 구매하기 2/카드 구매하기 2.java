7cc033b39ec9c933d4f5fac70bece747a9deac78
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-10-28 12:33:55 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 148 ms, Memory: 14520 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = arr[i - 1];
+            for (int j = 1; j <= i / 2; j++) {
+                dp[i] = Math.min(dp[i], dp[j] + dp[i - j]);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
