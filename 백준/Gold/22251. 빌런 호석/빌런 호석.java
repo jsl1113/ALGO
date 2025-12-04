7ed366038fd75787ad34300503dded4749f221d9
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-12-04 19:02:04 +0900

    [Gold V] Title: 빌런 호석, Time: 124 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, k, p, x, answer;
+    static int[][] arr = {
+            {0, 4, 3, 3, 4, 3, 2, 3, 1, 2},
+            {4, 0, 5, 3, 2, 5, 6, 1, 5, 4},
+            {3, 5, 0, 2, 5, 4, 3, 4, 2, 3},
+            {3, 3, 2, 0, 3, 2, 3, 2, 2, 1},
+            {4, 2, 5, 3, 0, 3, 4, 3, 3, 2},
+            {3, 5, 4, 2, 3, 0, 1, 4, 2, 1},
+            {2, 6, 3, 3, 4, 1, 0, 5, 1, 2},
+            {3, 1, 4, 2, 3, 4, 5, 0, 4, 3},
+            {1, 5, 2, 2, 3, 2, 1, 4, 0, 1},
+            {2, 4, 3, 1, 2, 1, 2, 3, 1, 0}
+    };
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        p = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
+
+        func(0, 1, 0, 0);
+
+        System.out.println(answer - 1);
+    }
+
+    static void func(int cnt, int ten, int now, int sum) {
+        if (now > n || sum > p) return;
+
+        if (cnt == k) {
+            if (now != 0) answer++;
+            return;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            func(cnt + 1, ten * 10, i * ten + now, sum + arr[x / ten % 10][i]);
+        }
+    }
+}
