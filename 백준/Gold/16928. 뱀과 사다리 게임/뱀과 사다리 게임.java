0c990ee659cf91096b752581fe80b5a0aa6c2460
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-12-04 18:50:40 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[101];
+        boolean[] visited = new boolean[101];
+        int[] answer = new int[101];
+
+        for (int i = 0; i < n + m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            arr[u] = v;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        answer[1] = 0;
+        visited[1] = true;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            if (current == 100) {
+                System.out.println(answer[current]);
+                return;
+            }
+
+            for (int i = 1; i <= 6; i++) {
+                int next = current + i;
+
+                if(100 < next || visited[next]) continue;
+
+                if (arr[next] != 0) {
+                    if (!visited[arr[next]]) {
+                        visited[arr[next]] = true;
+                        answer[arr[next]] = answer[current] + 1;
+                        queue.add(arr[next]);
+                    }
+                } else {
+                    visited[next] = true;
+                    answer[next] = answer[current] + 1;
+                    queue.add(next);
+                }
+            }
+        }
+    }
+}
