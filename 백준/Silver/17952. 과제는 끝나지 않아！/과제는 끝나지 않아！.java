7484c7b4ef56ef3db0df6f8393f59a254e662661
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-12-04 18:54:01 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 652 ms, Memory: 194904 KB -BaekjoonHub

---
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Stack<int[]> stack = new Stack<>();
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            if ("1".equals(st.nextToken())) {
+                stack.push(new int[]{
+                        Integer.parseInt(st.nextToken()), 
+                        Integer.parseInt(st.nextToken())
+                });
+            }
+
+            if (!stack.isEmpty() && --stack.peek()[1] == 0) {
+                answer += stack.pop()[0];
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
