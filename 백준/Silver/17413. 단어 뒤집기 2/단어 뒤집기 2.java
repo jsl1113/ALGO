90f2cef1af8c6ef1df1779be455f084d93c95f6c
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2025-12-04 18:53:19 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 168 ms, Memory: 16460 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+        StringBuilder tmp = new StringBuilder();
+
+        int i = 0;
+        while (i < str.length()) {
+            char c = str.charAt(i);
+
+            if (c == '<') {
+                sb.append(tmp.reverse());
+                tmp.setLength(0);
+                while (i < str.length() && str.charAt(i) != '>') {
+                    sb.append(str.charAt(i++));
+                }
+                sb.append('>');
+                i++;
+            } else if (c == ' ') {
+                sb.append(tmp.reverse()).append(' ');
+                tmp.setLength(0);
+                i++;
+            } else {
+                tmp.append(c);
+                i++;
+            }
+        }
+
+        sb.append(tmp.reverse());
+        System.out.println(sb);
+    }
+}
