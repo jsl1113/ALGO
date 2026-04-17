60969428f28e4822c2709938687d0275592d1f73
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2026-04-17 14:23:43 +0900

    [level 2] Title: 리프 노드 수 최대화, Time: 33.00 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/468372. 리프 노드 수 최대화/리프 노드 수 최대화.java
+++ 프로그래머스/2/468372. 리프 노드 수 최대화/리프 노드 수 최대화.java
@@ -1,25 +1,6 @@
 class Solution {
-   int answer = 1;
-	
-	public int solution(int dist_limit, int split_limit) {
-		answer = 1;
-		dfs(1, 1, 1, 0, dist_limit, split_limit);
+    public int solution(int dist_limit, int split_limit) {
+        int answer = 0;
         return answer;
-    }
-    
-	void dfs(long cur, long cnt, long split, long leaf, int limit, int splitLimit) {
-		if (cnt > limit) return;
-		
-		answer = (int) Math.max(answer, leaf + cur);
-		
-		for (int child = 2; child <= 3; child++) {
-			if (split * child > splitLimit) continue;
-            
-			long next = split * child;
-			long nextCur = Math.min(cur * child, limit - cnt);
-			long nextLeaf = leaf + (cur * child - nextCur);
-			
-			dfs(nextCur, cnt + nextCur, next, nextLeaf, limit, splitLimit);
-		}
     }
 }
